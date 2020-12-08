4564d005cef924c329def26e7e3d5c3aa4d117ac
Seeram Mounika (DAAI) <SE20020039@wipro.com>
2020-12-08 18:13:08 +0530

    Added Exercise1

---
+++ Exer1ApniDictonary.py
@@ -0,0 +1,5 @@
+dic = {"Love":"Love means the supreme god,Krishna","Google":"https://google.com Google is my dream company and also my future working company",
+       "Health":"Health is of 2 types, physical health, mental health"}
+
+search = input("Enter the word to know its meaning in Mounika's dictonary")
+print(dic[search])
